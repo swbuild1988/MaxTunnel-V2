55f606ba7f69b24cf127feb6fcd5d6a500dcf64b
krifyFan <suranmuge@126.com>
2019-10-12 09:29:41 +0800

    new maxTunnel-V2-front-end

---
+++ front-end/maxtunnel-v2-web/src/views/VM/Login/Login/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="Login-wrap">
+        {{data.pageName}}
+    </div>
+</template>
+
+<script lang="ts" src="./Login.ts"></script>
+
+<style lang="less">
+    @import './Login.less';
+</style>
+
